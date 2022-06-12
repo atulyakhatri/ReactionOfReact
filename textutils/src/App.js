675184a349759b929672d7dec0f6b7a8dc963c73
Atulya Khatri <65857938+atulyakhatri@users.noreply.github.com>
2022-06-12 17:41:14 +0000

    Played with App.js

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+let name = "Khatri";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <nav>
+      <li>Home</li>
+      <li> About </li>
+      <li> contact</li>
+    </nav>
+    <img scr="" alt="" />
+    <div className="container">
+      <h1> Hello from {name}</h1>
+      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quia reiciendis dolores ipsum culpa est pariatur repellat quo natus accusantium possimus, blanditiis fugit omnis fuga veritatis inventore rerum nam aliquam perspiciatis optio tempore eos.</p>
     </div>
+    </>
   );
 }
 
