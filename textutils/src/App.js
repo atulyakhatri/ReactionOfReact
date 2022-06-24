2549ad14c161487f28e87e2e0ab03098631963ca
Atulya Khatri <65857938+atulyakhatri@users.noreply.github.com>
2022-06-24 13:03:22 +0000

    Added dark mode

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,21 +1,38 @@
 // import logo from './logo.svg';
 import Navbar from './components/Navbar';
 import Textbar from './components/TextForm';
+// import About from './components/About';
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("light");
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+    }
+    else {
+      setMode("light")
+      document.body.style.backgroundColor = "white";
+    }
+  }
+
+
   return (
     <>
 
-      <Navbar />
+      <Navbar title="TextUtilss" mode={mode} toggleMode={toggleMode} />
       {/* <Navbar title = "TextUtilss"/> */}
       <div className='container my-3'>
-      <Textbar heading= "Enter the text to analyze"/>
+        <Textbar heading="Enter the text to analyze" mode={mode}/>
+        {/* <About /> */}
       </div>
-      
+
 
     </>
-    
+
   );
 }
 
--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -8,24 +8,38 @@ export default function Navbar(props) {
     setText(newText);
   }
 
+  const handleLowClick = () =>{
+    // console.log("Converted to UpperCase" + text);
+    let newText = text.toLowerCase();
+    setText(newText);
+  }
+
   const handleOnChange =(event) =>{
     // console.log("On change");
     setText(event.target.value);
   }
 
 
-  const[text, setText] = useState("Enter text here");
+  const[text, setText] = useState("");
   return (
-  <div>
-  <div className="mb-3">
-    <h1>{props.heading}</h1>
-    <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
+    <>
+  <div className="container" style= {{color: props.mode==="dark"?"white" : "#042743"}}>
+    <h2>{props.heading}</h2>
+    <div className='mb-3'>
+    <textarea className="form-control" value={text} onChange={handleOnChange} style= {{backgroundColor: props.mode==="dark"?"grey" : "white",color: props.mode==="dark"?"white" : "#042743"}}id="exampleFormControlTextarea1" rows="8"></textarea>
     
   </div>
-  <button className='btn btn-primary' onClick={handleUpClick}>Convert to UpperCase</button>
+  <button className='btn btn-primary mx-1' onClick={handleUpClick}>Convert to UpperCase</button>
+  <button className='btn btn-primary mx-1' onClick={handleLowClick}>Convert to LowerCase</button>
   </div>
-  
-  
+  <div className="container my-3" style= {{color: props.mode==="dark"?"white" : "#042743"}}>
+    <h2>Your text summary </h2>
+    <p>{text.trim().split(" ").length} words and {text.length} characters</p>
+    <p>{0.008 * text.trim().split(" ").length} Minutes read</p>
+    <h2>Preview</h2>
+    <p>{text.length>0?text:"Enter something to preview it here"}</p>
+  </div>
+  </>
   
   );
 }
